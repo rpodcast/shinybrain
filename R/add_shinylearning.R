daa52dbdad34a93baeeab559358f716e60b6a4ee
Eric Nantz <thercast@gmail.com>
2022-01-07 04:07:37 +0000

    add flag for creating RStudio projects in app creation

--- R/add_shinylearning.R
+++ R/add_shinylearning.R
@@ -1,17 +1,23 @@
 #' Initialize fresh shiny learning app
 #' @param app_name string of the app name to use as the directory
 #'   to store application files
-#' @param path parent directory to store app files. Default is the 
+#' @param path parent directory to store app files. Default is the
 #'   current working directory
+#' @param create_project Flag to create an RStudio project file
+#'   in the application directory. Default is FALSE.
 #' @param open Flag to open the project if using RStudio. Default
 #'   is TRUE.
 #' @param ... additional arguments to be used later
-#' 
+#'
 #' @return invisibly the path to new application
-add_shinylearning <- function(app_name, path = getwd(), open = TRUE, ...) {
+add_shinylearning <- function(
+  app_name, 
+  path = getwd(), 
+  create_project = FALSE,
+  open = TRUE, ...) {
   # define path variables
   app_path <- fs::path(path, app_name)
-  
+
   # remove current copy if it exists already
   if (fs::dir_exists(app_path)) {
     fs::dir_delete(app_path)
@@ -46,16 +52,18 @@ add_shinylearning <- function(app_name, path = getwd(), open = TRUE, ...) {
   # remove placeholder file
   fs::file_delete(fs::path(app_path, "page1", "page.R"))
 
-  # create rstudio project file
-  usethis::create_project(app_path, rstudio = TRUE, open = FALSE)
-
-  # open project if using RStudio and flag is TRUE
-  # thank you {golem}!
-  if (isTRUE(open)) {
-    if (rstudioapi::isAvailable() & rstudioapi::hasFun("openProject")) {
-      rproj_file <- fs::path(app_path, glue::glue("{app_name}.Rproj"))
-      rstudioapi::openProject(path = rproj_file)
-    } 
+  # create rstudio project file if requested
+  if (create_project) {
+    usethis::create_project(app_path, rstudio = TRUE, open = FALSE)
+    # open project if using RStudio and flag is TRUE
+    # thank you {golem}!
+    
+    if (isTRUE(open)) {
+      if (rstudioapi::isAvailable() & rstudioapi::hasFun("openProject")) {
+        rproj_file <- fs::path(app_path, glue::glue("{app_name}.Rproj"))
+        rstudioapi::openProject(path = rproj_file)
+      }
+    }
   }
 
   # return app path
