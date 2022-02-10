9f03c412787e133f629b1378a4b910ab2690faf6
Eric Nantz <thercast@gmail.com>
2022-02-10 03:48:13 +0000

    remove duplicated page function from template

--- inst/templates/page/page.R
+++ inst/templates/page/page.R
@@ -28,20 +28,3 @@ page{{app_snapshot}}_server <- function(input, output, session) {
     random_plot
   })
 }
-
-page{{app_snapshot}} <- function(nav_links) {
-  page(
-    href = "/page{{app_snapshot}}",
-    ui = function(request) {
-      tagList(
-        tags$head(
-          includeCSS("www/navigation.css")
-        ),
-        page{{app_snapshot}}_ui(nav_links)
-      )
-    },
-    server = function(input, output, session) {
-      page{{app_snapshot}}_server(input, output, session)
-    }
-  )
-}
