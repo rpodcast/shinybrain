8b206de20de9d21b5063c7226e9639e7544c2c26
Eric Nantz <thercast@gmail.com>
2022-03-11 04:18:40 +0000

    add the custom app_page function back (that was close)!

--- R/utils.R
+++ R/utils.R
@@ -26,3 +26,37 @@ fill_template <- function(app_path, template_file, output_file, app_title, app_s
 
   invisible(TRUE)
 }
+
+#' Compose single page of shinylearning app
+#' @param nav_links list of navigation bar elements created with `gen_navitems`.
+#' @param page_id page identifer.
+#' @param title application title
+#' @param bg color of the navigation bar
+#' @return list produced by `brochure::page`
+#' @export
+app_page <- function(nav_links, page_id = "page1", title = "myapp", bg = "#0062cc") {
+  if (page_id == "home") {
+    href <- "/"
+  } else {
+    href <- paste0("/", page_id)
+  }
+
+  brochure::page(
+    href = href,
+    ui = function(request) {
+      tagList(
+        bslib::page_navbar(
+          title = title,
+          bg = bg,
+          !!!nav_links,
+          footer = div(
+            rlang::exec(paste(page_id, "ui", sep = "_"))
+          )
+        )
+      )
+    },
+    server = function(input, output, session) {
+      rlang::exec(paste(page_id, "server", sep = "_"), input, output, session)
+    }
+  )
+}
